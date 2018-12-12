3f732722f447952b395a45f9ba94f82d17ca58e4
six.nonacosa <pkwenda@163.com>
2018-12-12 16:44:38 +0800

    goroutine

--- src/main.go
+++ src/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/pkwenda/asJob/src/download"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+	for i:=0; i<4; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			if err:= download.Spider(); err != nil {
+				fmt.Print(err)
+			}
+		}(i)
 
-	download.Worker(1)
-
+	}
+	wg.Wait()
 
 }
